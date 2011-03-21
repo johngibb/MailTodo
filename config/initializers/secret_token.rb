bf1608d47dcf43a36025f40a147933f51dd4033a
John Gibb <johngibb@gmail.com>
2011-03-21 13:32:32 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailtodo::Application.config.secret_token = '9e32ed2d0660b10961e9d2082e3ef08b396d52cb5bc17b70f28e00fd834be4a6fcdd98667cffc00ca66a69dd8c69735402fd580526b606bfc6e93559b9937b8d'
