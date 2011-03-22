9effe02c8f97038b2aaae70e5f747ef9b71b282d
John Gibb <johngibb@gmail.com>
2011-03-21 17:34:52 -0700

    save todo on email received

--- app/controllers/listener_controller.rb
+++ app/controllers/listener_controller.rb
@@ -1,5 +1,15 @@
 class ListenerController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+    
   def receive_email
+    user = User.find_or_create_by_email!(params[:from])
+    
+    Todo.create!(
+      :user => user,
+      :title => params[:subject],
+      :description => params[:html]
+    )
+    
     render :inline => 'hello, world!'
   end
 end
