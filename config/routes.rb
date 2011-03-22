59053f95096055bb15022788ea272b22b1b1a87c
John Gibb <johngibb@gmail.com>
2011-03-21 17:28:39 -0700

    Add controller to show Todos for a given user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Mailtodo::Application.routes.draw do
+  
   match '/listener/receive_email' => 'listener#receive_email'
+  
+  resources :users, :only => [] do
+    resources :todos, :only => :index
+  end
+  
 end
