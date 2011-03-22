266317aa7cc3338765e7d160d05a56e6dd06c29e
John Gibb <johngibb@gmail.com>
2011-03-21 17:32:04 -0700

    Find and show all todos for the given user

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,14 @@
 class TodosController < ApplicationController
-
+  before_filter :find_user
+  
   def index
-
+    @todos = @user.todos
+  end
+  
+  private
+  
+  def find_user
+    @user = User.find params[:user_id] if params.key? :user_id
   end
 
 end
