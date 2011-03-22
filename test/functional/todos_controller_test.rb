4f6f2cd7d3130b9c7a9100d7076bdcfa1f65c24f
John Gibb <johngibb@gmail.com>
2011-03-21 18:18:39 -0700

    test covering viewing todos

--- test/functional/todos_controller_test.rb
+++ test/functional/todos_controller_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class TodosControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "Listing Todos" do
+    user = users(:one)
+    get :index, :user_id => user.to_param
+    
+    assert_response :success
+    assert_not_nil assigns(:user)
+    assert_not_nil assigns(:todos)
   end
 end
